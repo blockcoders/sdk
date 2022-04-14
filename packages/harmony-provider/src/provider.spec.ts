0bd394fb10f740f29bbd3951a5425638f7425a83
Fernando Sirni <fsirni@gmail.com>
2022-04-14 15:28:56 -0300

    lint, tests and some new methods on provider

--- packages/harmony-provider/src/provider.spec.ts
+++ packages/harmony-provider/src/provider.spec.ts
@@ -1,223 +1,237 @@
-import { HarmonyProvider } from "./provider"
-import { HMY_TESTNET_NETWORK } from "./network"
+import { fail } from 'assert'
 import { expect } from 'chai'
-import { fail } from "assert"
+import { HMY_TESTNET_NETWORK } from './network'
+import { HarmonyProvider, HarmonyProviderMethods } from './provider'
 
 const BLOCK_NUMBER_HEX = '0x47db8e'
-const BLOCK_NUMBER = parseInt(BLOCK_NUMBER_HEX,16)
+const BLOCK_NUMBER = parseInt(BLOCK_NUMBER_HEX, 16)
 const BLOCK_HASH = '0xd5258b03ae6b7d1ad8e932bf1aed6257241101e98be7ac6dab74013f267596de'
 const TX_HASH = '0x371ec289c5973c16d58cf600f8f2ce040fe8b429a6007976040d55a1759b8993'
 
-describe("Harmony Provider Tests", () => {
+describe('Harmony Provider Tests', () => {
   let provider: HarmonyProvider
 
   before(() => {
     provider = new HarmonyProvider(HMY_TESTNET_NETWORK)
   })
 
-  it("has to exists", () => {
+  it('has to exists', () => {
     expect(provider).to.not.be.undefined
   })
 
-  describe("Get block number", () => {
-    it("gets the block number", async () => {
-      const response = await provider.perform("getBlockNumber", [])
-      expect(response).to.be.a("number")
+  describe('Get block number', () => {
+    it('gets the block number', async () => {
+      const response = await provider.getBlockNumber()
+      expect(response).to.be.a('number')
     })
   })
 
-  describe("Get block by hash or number(int or hex)", () => {
-
-    it("returns null if there are no valid params", async () => {
-      const response = await provider.perform("getBlock", {})
-      expect(response).to.be.null
+  describe('Get block by hash or number(int or hex)', () => {
+    it('returns null if there are no valid params', async () => {
+      try {
+        await provider.getBlockByTagOrHash({})
+        fail('Should fail while trying to getBlockByNumber with invalid parameters')
+      } catch (error) {
+        expect(error).to.not.be.undefined
+      }
     })
 
     it("get the last block using the 'latest' value", async () => {
       const params = {
-        blockTag: "latest",
+        blockTag: 'latest',
       }
-      const response = await provider.perform("getBlock", params)
+      const response = await provider.getBlockByTagOrHash(params)
       expect(response).to.not.be.null
-      expect(response).to.haveOwnProperty("number")
-      expect(response).to.haveOwnProperty("hash")
-      expect(response).to.haveOwnProperty("parentHash")
-      expect(response).to.haveOwnProperty("nonce")
-      expect(response).to.haveOwnProperty("mixHash")
-      expect(response).to.haveOwnProperty("logsBloom")
-      expect(response).to.haveOwnProperty("stateRoot")
-      expect(response).to.haveOwnProperty("miner")
-      expect(response).to.haveOwnProperty("difficulty")
-      expect(response).to.haveOwnProperty("extraData")
-      expect(response).to.haveOwnProperty("size")
-      expect(response).to.haveOwnProperty("gasLimit")
-      expect(response).to.haveOwnProperty("gasUsed")
-      expect(response).to.haveOwnProperty("vrf")
-      expect(response).to.haveOwnProperty("vrfProof")
-      expect(response).to.haveOwnProperty("timestamp")
-      expect(response).to.haveOwnProperty("transactionsRoot")
-      expect(response).to.haveOwnProperty("receiptsRoot")
-      expect(response).to.haveOwnProperty("uncles")
-      expect(response).to.haveOwnProperty("transactions")
-      expect(response).to.haveOwnProperty("transactionsInEthHash")
-      expect(response).to.haveOwnProperty("stakingTransactions")
-
+      expect(response).to.haveOwnProperty('number')
+      expect(response).to.haveOwnProperty('hash')
+      expect(response).to.haveOwnProperty('parentHash')
+      expect(response).to.haveOwnProperty('nonce')
+      expect(response).to.haveOwnProperty('mixHash')
+      expect(response).to.haveOwnProperty('logsBloom')
+      expect(response).to.haveOwnProperty('stateRoot')
+      expect(response).to.haveOwnProperty('miner')
+      expect(response).to.haveOwnProperty('difficulty')
+      expect(response).to.haveOwnProperty('extraData')
+      expect(response).to.haveOwnProperty('size')
+      expect(response).to.haveOwnProperty('gasLimit')
+      expect(response).to.haveOwnProperty('gasUsed')
+      expect(response).to.haveOwnProperty('vrf')
+      expect(response).to.haveOwnProperty('vrfProof')
+      expect(response).to.haveOwnProperty('timestamp')
+      expect(response).to.haveOwnProperty('transactionsRoot')
+      expect(response).to.haveOwnProperty('receiptsRoot')
+      expect(response).to.haveOwnProperty('uncles')
+      expect(response).to.haveOwnProperty('transactions')
+      expect(response).to.haveOwnProperty('transactionsInEthHash')
+      expect(response).to.haveOwnProperty('stakingTransactions')
     })
 
-    it("fails when the blockTag property (used to find by number) is invalid", async () => {      
+    it('fails when the blockTag property (used to find by number) is invalid', async () => {
       try {
         const params = {
-          blockTag: "invalid",
+          blockTag: 'invalid',
         }
-        await provider.perform("getBlock", params)
-        fail("Should fail while trying to getBlockByNumber with an invalid number")
+        await provider.getBlockByTagOrHash(params)
+        fail('Should fail while trying to getBlockByNumber with an invalid number')
       } catch (error) {
         expect(error).to.not.be.undefined
       }
     })
 
-    it("fails when the blockHash property (used to find by hash) is invalid", async () => {
+    it('fails when the blockHash property (used to find by hash) is invalid', async () => {
       try {
         const params = {
-          blockHash: "invalid",
+          blockHash: 'invalid',
         }
-        await provider.perform("getBlock", params)
-        fail("Should fail while trying to getBlockByHash with an invalid hash")
+        await provider.getBlockByTagOrHash(params)
+        fail('Should fail while trying to getBlockByHash with an invalid hash')
       } catch (error) {
         expect(error).to.not.be.undefined
       }
     })
 
-    it("gets the block without txs by hex number without sending the includeTransactions property", async () => {
+    it('gets the block without txs by hex number without sending the includeTransactions property', async () => {
       const params = {
         blockTag: BLOCK_NUMBER_HEX,
       }
-      const response = await provider.perform("getBlock", params)
+      const response = await provider.getBlockByTagOrHash(params)
       expect(response).to.not.be.null
       expect(response.number).to.equal(BLOCK_NUMBER_HEX)
       expect(response.transactions[0]).to.equal(TX_HASH)
     })
 
-    it("gets the block without txs by number without sending the includeTransactions property", async () => {
+    it('gets the block without txs by number without sending the includeTransactions property', async () => {
       const params = {
         blockTag: BLOCK_NUMBER,
       }
-      const response = await provider.perform("getBlock", params)
+      const response = await provider.getBlockByTagOrHash(params)
       expect(response).to.not.be.null
       expect(response.number).to.equal(BLOCK_NUMBER_HEX)
       expect(response.transactions[0]).to.equal(TX_HASH)
     })
 
-    it("gets the block without txs by hash without sending the includeTransactions property", async () => {
+    it('gets the block without txs by hash without sending the includeTransactions property', async () => {
       const params = {
         blockHash: BLOCK_HASH,
       }
-      const response = await provider.perform("getBlock", params)
+      const response = await provider.getBlockByTagOrHash(params)
       expect(response).to.not.be.null
       expect(response.number).to.equal(BLOCK_NUMBER_HEX)
       expect(response.transactions[0]).to.equal(TX_HASH)
     })
-  
-    it("gets the block with txs by hex number", async () => {
+
+    it('gets the block with txs by hex number', async () => {
       const params = {
         blockTag: BLOCK_NUMBER_HEX,
-        includeTransactions: true
       }
-      const response = await provider.perform("getBlock", params)
+      const response = await provider.getBlockByTagOrHash(params, true)
       expect(response).to.not.be.null
       expect(response.number).to.equal(BLOCK_NUMBER_HEX)
-      expect(response.transactions[0]).to.haveOwnProperty("hash")
+      expect(response.transactions[0]).to.haveOwnProperty('hash')
       expect(response.transactions[0].hash).to.equal(TX_HASH)
     })
-  
-    it("gets the block without txs by hex number", async () => {
+
+    it('gets the block without txs by hex number', async () => {
       const params = {
         blockTag: BLOCK_NUMBER_HEX,
-        includeTransactions: false
       }
-      const response = await provider.perform("getBlock", params)
+      const response = await provider.getBlockByTagOrHash(params, false)
       expect(response).to.not.be.null
       expect(response.number).to.equal(BLOCK_NUMBER_HEX)
       expect(response.transactions[0]).to.equal(TX_HASH)
     })
-  
-    it("gets the block with txs by number", async () => {
+
+    it('gets the block with txs by number', async () => {
       const params = {
         blockTag: BLOCK_NUMBER,
-        includeTransactions: true
       }
-      const response = await provider.perform("getBlock", params)
+      const response = await provider.getBlockByTagOrHash(params, true)
       expect(response).to.not.be.null
       expect(response.number).to.equal(BLOCK_NUMBER_HEX)
-      expect(response.transactions[0]).to.haveOwnProperty("hash")
+      expect(response.transactions[0]).to.haveOwnProperty('hash')
       expect(response.transactions[0].hash).to.equal(TX_HASH)
     })
-  
-    it("gets the block without txs by number", async () => {
+
+    it('gets the block without txs by number', async () => {
       const params = {
         blockTag: BLOCK_NUMBER,
-        includeTransactions: false
       }
-      const response = await provider.perform("getBlock", params)
+      const response = await provider.getBlockByTagOrHash(params, false)
       expect(response).to.not.be.null
       expect(response.number).to.equal(BLOCK_NUMBER_HEX)
       expect(response.transactions[0]).to.equal(TX_HASH)
     })
-  
-    it("gets the block with txs by hash", async () => {
+
+    it('gets the block with txs by hash', async () => {
       const params = {
         blockHash: BLOCK_HASH,
-        includeTransactions: true
       }
-      const response = await provider.perform("getBlock", params)
+      const response = await await provider.getBlockByTagOrHash(params, true)
       expect(response).to.not.be.null
       expect(response.hash).to.equal(BLOCK_HASH)
-      expect(response.transactions[0]).to.haveOwnProperty("hash")
+      expect(response.transactions[0]).to.haveOwnProperty('hash')
       expect(response.transactions[0].hash).to.equal(TX_HASH)
     })
-  
-    it("gets the block without txs by hash", async () => {
+
+    it('gets the block without txs by hash', async () => {
       const params = {
         blockHash: BLOCK_HASH,
-        includeTransactions: false
       }
-      const response = await provider.perform("getBlock", params)
+      const response = await provider.getBlockByTagOrHash(params, false)
       expect(response).to.not.be.null
       expect(response.hash).to.equal(BLOCK_HASH)
       expect(response.transactions[0]).to.equal(TX_HASH)
     })
   })
 
-  describe("Get transaction", () => {
-    it("gets the transaction by hash", async () => {
-      const params = {
-        transactionHash: TX_HASH
-      }
-      const response = await provider.perform("getTransaction", params)
+  describe('Get transaction', () => {
+    it('gets the transaction by hash', async () => {
+      const response = await provider.getTransaction(TX_HASH)
       expect(response).to.not.be.undefined
-      expect(response).to.haveOwnProperty("hash")
+      expect(response).to.haveOwnProperty('hash')
       expect(response.hash).to.equal(TX_HASH)
     })
 
-    it("fails when the txHash property is invalid", async () => {
+    it('fails when the txHash property is invalid', async () => {
       try {
-        const params = {
-          transactionHash: "invalid",
-        }
-        await provider.perform("getTransaction", params)
-        fail("Should fail while trying to get the transaction with an invalid hash")
+        await provider.getTransaction('invalid')
+        fail('Should fail while trying to get the transaction with an invalid hash')
       } catch (error) {
         expect(error).to.not.be.undefined
       }
     })
+  })
 
-    it("fails when the txHash property is undefined", async () => {
-      try {
-        await provider.perform("getTransaction", {})
-        fail("Should fail while trying to get the transaction with an invalid hash")
-      } catch (error) {
-        expect(error).to.not.be.undefined
-      }
+  describe('Get block number, block and transaction from perform', () => {
+    it('gets the transaction by hash', async () => {
+      const params = { transactionHash: TX_HASH }
+      const response = await provider.perform(HarmonyProviderMethods.getTransaction, params)
+      expect(response).to.not.be.undefined
+      expect(response).to.haveOwnProperty('hash')
+      expect(response.hash).to.equal(TX_HASH)
+    })
+
+    it('gets the block by hash', async () => {
+      const params = { blockHash: BLOCK_HASH, includeTransactions: true }
+      const response = await provider.perform(HarmonyProviderMethods.getBlock, params)
+      expect(response).to.not.be.undefined
+      expect(response).to.haveOwnProperty('hash')
+      expect(response).to.haveOwnProperty('transactions')
+      expect(response.hash).to.equal(BLOCK_HASH)
+    })
+
+    it('gets the block by number', async () => {
+      const params = { blockTag: BLOCK_NUMBER, includeTransactions: false }
+      const response = await provider.perform(HarmonyProviderMethods.getBlock, params)
+      expect(response).to.not.be.undefined
+      expect(response).to.haveOwnProperty('hash')
+      expect(response).to.haveOwnProperty('transactions')
+      expect(response.number).to.equal(BLOCK_NUMBER_HEX)
+    })
+
+    it('gets the block number', async () => {
+      const response = await provider.perform(HarmonyProviderMethods.getBlockNumber, [])
+      expect(response).to.be.a('number')
     })
   })
 })
--- packages/harmony-provider/src/provider.ts
+++ packages/harmony-provider/src/provider.ts
@@ -1,10 +1,6 @@
-import {
-  JsonRpcProvider,
-  Network,
-  Networkish,
-} from '@ethersproject/providers'
-import { fetchJson } from '@ethersproject/web'
 import { deepCopy } from '@ethersproject/properties'
+import { JsonRpcProvider, Network, Networkish, TransactionResponse } from '@ethersproject/providers'
+import { fetchJson } from '@ethersproject/web'
 import { getNetwork } from './network'
 
 function getResult(payload: any): any {
@@ -16,8 +12,18 @@ function getResult(payload: any): any {
 const HARMONY_MAINNET_URL = 'https://api.harmony.one'
 const HARMONY_TESTNET_URL = 'https://api.s0.b.hmny.io'
 
-export class HarmonyProvider extends JsonRpcProvider {
+export enum HarmonyProviderMethods {
+  getBlockNumber = 'getBlockNumber',
+  getBlock = 'getBlock',
+  getTransaction = 'getTransaction',
+}
 
+interface blockParams {
+  blockTag?: string | number
+  blockHash?: string
+}
+
+export class HarmonyProvider extends JsonRpcProvider {
   constructor(_network?: Networkish) {
     const network = getNetwork(_network)
     const baseUrl = HarmonyProvider.getBaseUrl(network)
@@ -28,25 +34,37 @@ export class HarmonyProvider extends JsonRpcProvider {
 
   async perform(method: string, params: Record<string, any>): Promise<any> {
     switch (method) {
-      case 'getBlock':
-        const includeTransactions: boolean = params?.includeTransactions || false
-        if(params.blockTag) {
-          return this.send('hmy_getBlockByNumber', [params.blockTag, includeTransactions])
-        } else if(params.blockHash) {
-          return this.send('hmy_getBlockByHash', [params.blockHash, includeTransactions])
-        }
-        return null
-      case 'getBlockNumber':
-        const hexBlockNumber = await this.send('hmy_blockNumber',[])
-        if(!hexBlockNumber) {
-          throw Error("Error while getting the block number")
-        }
-        return parseInt(hexBlockNumber,16)
-      case "getTransaction":
-        return this.send("hmy_getTransactionByHash", [ params.transactionHash ])
+      case HarmonyProviderMethods.getBlock:
+        return this.getBlockByTagOrHash(params, params.includeTransactions)
+      case HarmonyProviderMethods.getBlockNumber:
+        return this.getBlockNumber()
+      case HarmonyProviderMethods.getTransaction:
+        return this.getTransaction(params.transactionHash)
       default:
         return super.perform(method, params)
     }
+  }
+
+  async getTransaction(hash: string): Promise<TransactionResponse> {
+    return this.send('hmy_getTransactionByHash', [hash])
+  }
+
+  async getBlockByTagOrHash(params: blockParams, includeTransactions = false): Promise<any> {
+    if (params.blockTag) {
+      return this.send('hmy_getBlockByNumber', [params.blockTag, includeTransactions])
+    } else if (params.blockHash) {
+      return this.send('hmy_getBlockByHash', [params.blockHash, includeTransactions])
+    } else {
+      throw Error(`Invalid parameters: ${params}`)
+    }
+  }
+
+  async getBlockNumber(): Promise<number> {
+    const hexBlockNumber = await this.send('hmy_blockNumber', [])
+    if (!hexBlockNumber) {
+      throw Error('Error while getting the block number')
+    }
+    return parseInt(hexBlockNumber, 16)
   }
 
   static getBaseUrl(network?: Network | null): string {
@@ -113,5 +131,4 @@ export class HarmonyProvider extends JsonRpcProvider {
       throw err
     }
   }
-
 }
